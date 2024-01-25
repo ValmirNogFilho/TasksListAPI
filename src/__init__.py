7a75ab51c43d6d8b8dd122777c97e69e6edbd703
ValmirNogFilho <valmirnogueira514@gmail.com>
2024-01-25 11:51:33 -0300

    :bug: fix: allowing front-end to access API

--- src/__init__.py
+++ src/__init__.py
@@ -8,6 +8,13 @@ app.config["SECRET_KEY"] = "1fbc7cd09f3e7b4d3f9c6eed"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
 db = SQLAlchemy(app)
-CORS(app, supports_credentials=True)
+
+rsc = {
+    r'/*' : {
+        'origins': 'https://tasks-list-one.vercel.app'
+    }
+}
+
+CORS(app, supports_credentials=True, resources=rsc)
 
 from src import routes
